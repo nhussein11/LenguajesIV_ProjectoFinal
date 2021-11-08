23d038a8e4d2a0f63da793e35e50e36c3a1d1ad4
T0w3r <torres.alejo2001@gmail.com>
2021-11-08 18:19:22 -0300

    Agregado pantalla final del proceso registrar multa

---
+++ LenguajesIV_ProjectoFinal/Views/MarcarEnElMapa.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LenguajesIV_ProjectoFinal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MarcarEnElMapa : ContentPage
+    {
+        public MarcarEnElMapa()
+        {
+            InitializeComponent();
+        }
+
+        private void GrabarMulta(object sender, EventArgs e)
+        {
+            //Aca recupero todos los objetos desde properties vehiculo/cod_vehiculo infractor/cod_infractor multa y detalles
+            //aca van a estar los datos de la localizacion tambien
+            //los inserto a todos
+
+        }
+
+        private void Atras(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync($"//{nameof(CargarDetallesMulta)}");
+        }
+
+        
+    }
+}
