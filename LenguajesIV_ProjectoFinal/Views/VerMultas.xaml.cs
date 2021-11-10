80255b38e45651d6742b294946156c0bc1686b56
T0w3r <torres.alejo2001@gmail.com>
2021-11-10 16:52:09 -0300

    @nhussein11 falla en metodo de la ubicacion

--- LenguajesIV_ProjectoFinal/Views/VerMultas.xaml.cs
+++ LenguajesIV_ProjectoFinal/Views/VerMultas.xaml.cs
@@ -46,16 +46,25 @@ namespace LenguajesIV_ProjectoFinal.Views
             List<Multas> lista_multas = await App.SQLiteDB.Get_MultasxAgente_Async(cod_agente_activo);
             foreach (Multas multa in lista_multas)
             {
-                MultasRealizadas multaRealizada = new MultasRealizadas
+                string nombre_agente = ((Agentes)Application.Current.Properties["DatosUsuario"]).nombre_agente;
+                string dni_infractor = (await App.SQLiteDB.Get_InfractorxMulta_Async(multa.cod_infractor)).dni_infractor; //funciona bien
+                string patente_dominio_vehiculo = (await App.SQLiteDB.Get_VehiculoxMulta_Async(multa.cod_vehiculo)).patente_dominio_vehiculo; //funciona bien
+                string longitud_ubicacion = (await App.SQLiteDB.Get_UbicacionxMulta_Async(multa.cod_multa)).longitud_ubicacion; // aca esta fallando 
+                string apellido_agente = ((Agentes)Application.Current.Properties["DatosUsuario"]).apellido_agente;
+                string fecha_multa = multa.fecha_multa;
+                string latitud_ubicacion = (await App.SQLiteDB.Get_UbicacionxMulta_Async(multa.cod_multa)).latitud_ubicacion; // aca esta fallando
+                string lugar_multa = multa.lugar_multa;
+                /*Falla en las ubicaciones, las estas grabando en la bd? con el cod de la multa? el error que tira es porque devuevle null y no sabe a donde ir a buscar la propiedad de la latitud/longitud*/
+               MultasRealizadas multaRealizada = new MultasRealizadas
                 {
-                    nombre_agente = ((Agentes)Application.Current.Properties["DatosUsuario"]).nombre_agente,
-                    apellido_agente = ((Agentes)Application.Current.Properties["DatosUsuario"]).apellido_agente,
-                    fecha_multa = multa.fecha_multa,
-                    lugar_multa = multa.lugar_multa,
-                    dni_infractor = (await App.SQLiteDB.Get_InfractorxMulta_Async(multa.cod_infractor)).dni_infractor,
-                    patente_dominio_vehiculo = (await App.SQLiteDB.Get_VehiculoxMulta_Async(multa.cod_vehiculo)).patente_dominio_vehiculo,
-                    latitud_ubicacion= (await App.SQLiteDB.Get_UbicacionxMulta_Async(multa.cod_multa)).latitud_ubicacion,
-                    longitud_ubicacion = (await App.SQLiteDB.Get_UbicacionxMulta_Async(multa.cod_multa)).longitud_ubicacion,
+                    nombre_agente = nombre_agente,
+                    apellido_agente = apellido_agente,
+                    fecha_multa = fecha_multa,
+                    lugar_multa = lugar_multa,
+                    dni_infractor = dni_infractor,
+                    patente_dominio_vehiculo = patente_dominio_vehiculo,
+                    latitud_ubicacion= latitud_ubicacion,
+                    longitud_ubicacion = longitud_ubicacion
                 };
                 this.ListadoMultasRealizadas.Add(multaRealizada);
             }
@@ -94,7 +103,7 @@ namespace LenguajesIV_ProjectoFinal.Views
 
             await Shell.Current.GoToAsync($"//{nameof(Perfil)}"); //old but gold truco de "actualizar" vista
             await Shell.Current.GoToAsync($"//{nameof(VerMultas)}");
-            Cargar_Multas_Realizadas();
+            
             // esto desp lo ponemos en el constructor de la vista, por ahora hasta que lo arreglemos, queda en el boton 
         }
     }
